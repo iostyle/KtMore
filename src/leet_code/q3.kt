2b222595d9dc7b6dd6b21535ff8b348e9e660c95
iostyle <DeveloperSKY@163.com>
2019-09-30 15:42:40 +0800

    feat: LeetCode

---
+++ src/leet_code/q3.kt
@@ -0,0 +1,47 @@
+package leet_code
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * 示例 1:
+
+输入: "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+class q3 {
+    fun lengthOfLongestSubstring(s: String): Int {
+        val charArray = s.toCharArray()
+        if (charArray.size == 1) return 1
+        var size = 0
+        for (i in 0 until charArray.size - 1) {
+            var tempSize = 0
+            lable@ for (j in i + 1 until charArray.size) {
+                for (k in i until j)
+                    if (charArray[j] == charArray[k]) {
+                        tempSize = (j - i)
+                        break@lable
+                    }
+            }
+            if (tempSize == 0) tempSize = charArray.size - i
+            if (tempSize > size) size = tempSize
+        }
+        return size
+    }
+}
--- src/leet_code/run.kt
+++ src/leet_code/run.kt
@@ -4,5 +4,13 @@ fun main() {
 //    val intArray = intArrayOf(8, 2, 5, 6, 7)
 //    print(q1().twoSum(intArray, 9).toStr())
 
-    print(q2().addTwoNumbers(ListNode(5),ListNode(5))?.toStr())
+//    print(q2().addTwoNumbers(ListNode(5),ListNode(5))?.toStr())
+
+//    println(q3().lengthOfLongestSubstring("abcabcbb"))
+//    println(q3().lengthOfLongestSubstring(""))
+//    println(q3().lengthOfLongestSubstring(" "))
+//    println(q3().lengthOfLongestSubstring("au"))
+//    println(q3().lengthOfLongestSubstring("aab"))
+//    println(q3().lengthOfLongestSubstring("cdd"))
+//    println(q3().lengthOfLongestSubstring("pwwkew"))
 }
